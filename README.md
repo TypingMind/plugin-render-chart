1ce32482ed12e6f7817e2f96402c5a54a1e65eb0
Duy Nguyen <truongduy.2611@gmail.com>
2024-06-15 09:33:43 +0700

    feat: addd chart.js plugin

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# render-chart-js
+## Render Chart.js
+
+This is a plugin to render data using [Chart.js](https://www.chartjs.org/docs/latest/). 
+
+[Learn how to develop Typing Mind plugins here](https://www.typingmind.com/plugins-docs).
+
+This plugin demonstrates how to render Chart.js charts to the end users.
+
+### Example usage:
+
+> create a bar chart with the following data: labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"], data: [12, 19, 3, 5, 2, 3]
+
+> render a pie chart with these categories: ["Electronics", "Fashion", "Home & Garden", "Toys", "Sports"], values: [25, 15, 20, 10, 30]
---
+++ implementation.js
@@ -0,0 +1,38 @@
+function generate_chart_js(params) {
+    const { chartType, options, title, data } = params;
+    const dataString = JSON.stringify(data)
+        .replace(/\\/g, '\\\\')
+        .replace(/"/g, '\\"')
+        .replace(/\n/g, '\\n')
+        .replace(/\r/g, '\\r')
+        .replace(/\t/g, '\\t');
+
+    const optionsString = JSON.stringify(options)
+        .replace(/\\/g, '\\\\')
+        .replace(/"/g, '\\"')
+        .replace(/\n/g, '\\n')
+        .replace(/\r/g, '\\r')
+        .replace(/\t/g, '\\t');
+
+    const htmlString = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>${title}</title>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    </head>
+    <body>
+        <canvas id="generated-chart"></canvas>
+        <script>
+            var ctx = document.getElementById('generated-chart').getContext('2d');
+            var chart = new Chart(ctx, {
+                type: '${chartType}',
+                data: JSON.parse("${dataString}"),
+                options: JSON.parse("${optionsString}")
+            });
+        </script>
+    </body>
+    </html>
+    `;
+    return htmlString;
+}
