423ce0becc04599e90e13e1aa3687974f603a806
Tony Dinh <gh@tdinh.me>
2024-06-22 14:16:13 +0700

    Update implementation.js

--- implementation.js
+++ implementation.js
@@ -22,7 +22,7 @@ function render_chart(params) {
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     </head>
     <body>
-        <canvas id="generated-chart"></canvas>
+        <canvas id="generated-chart" style="width: 100%; height: 90vh;"></canvas>
         <script>
             var ctx = document.getElementById('generated-chart').getContext('2d');
             var chart = new Chart(ctx, {
